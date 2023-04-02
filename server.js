32b8d7e7ed64035b8683eedea77ce776f788a9e8
Ravi Shankar Yadav <raviiyadav.job@gmail.com>
2023-04-02 17:38:33 +0530

    final code
    E-Commerce Website (MERN STACK APP)

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectToMongo from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from "path";
+
+// configure dotenv
+dotenv.config();
+
+// database config
+connectToMongo();
+
+// rest object
+const app = express();
+
+// port
+const port = process.env.port || 8080;
+
+// middlewres
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+// rest api
+// app.get("/", (req, res) => {
+//   res.send("<h2>Welcome to Ecommerce website</h2>");
+// });
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//run listen
+app.listen(port, () => {
+  console.log(`Ecommerce backend listening on port ${port}`.bgCyan.white);
+});
